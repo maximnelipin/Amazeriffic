4affce2bde05d75a7c5ff36525a5f5df55f5a06b
Maxim Nelipin <nelipin.maxim@yandex.ru>
2015-05-24 18:13:21 +0300

    Добавили контент во вкладки
    Контент берётся из массива и выравнивается либо по возрастанию, либо по
    убыванию номера в массве

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,45 @@
 var main = function () {
 	"use strict";
-	var addCommentFromInputBox = function () { //функция добаления комментрия
-		var $new_comment;
-		if($(".comment-input input").val()!==""){//Если поле непустое		
-			$new_comment=$("<p>").text($(".comment-input input").val()); //создаем новый комментрий
-			$new_comment.hide(); //скрыли
-			$(".comments").append($new_comment); //добавили на страницу
-			$new_comment.fadeIn(); //плавно вывели
-			$(".comment-input input").val(""); //очмстили поле ввода
-			}
+	var toDos = [
+				"Закончить писать эту книгу",
+				"Вывести Грейси на прогулку в парк",
+				"Ответить на электронные письма",
+				"Подготовиться к лекции в понедельник",
+				"Обновить несколько новых задач",
+				"Купить продукты"
+				];
+	$(".tabs a span").toArray().forEach(function (element){
+		//обработчик щелчка
+		$(element).on("click", function () {
+			var $element = $(element),
+			$content;
+			//снимае флаг активности с вкладки
+			$(".tabs a span").removeClass("active");
+			//делаем активную вкладку, на которой щёлкнули
+			$element.addClass("active");
+			$("main .content").empty();
+			if ($element.parent().is(":nth-child(1)")){
+				$content=$("<ul>");
+					toDos.reduceRight(function (a,todo){
+						$content.append($("<li>").text(todo));
+					},0);
+					$("main .content").append($content);
+					}
+				else if ($element.parent().is(":nth-child(2)")){
+					$content=$("<ul>");
+					toDos.forEach(function (todo){
+						$content.append($("<li>").text(todo));
+					});
+					$("main .content").append($content);
+				}
+				else if ($element.parent().is(":nth-child(3)")) {
+						console.log("Щелчок на третьей вкладке!");
+					}
+			return false;
+		});
+			
+		});
+		$(".tabs a:first-child span").trigger("click");
 	};
-	$(".comment-input button").on("click", function (event) { //по нажатию на кнопку !!!! Обрабатываем кнопку BUTTON
-		addCommentFromInputBox();
-	});
-	$(".comment-input input").on("keypress", function (event) {
-		var $new_comment;
-		if (event.keyCode === 13) { //по нажатию на Enter !!!! Обрабатываем поле ввода INPUT
-			addCommentFromInputBox();
-		}
-	});
-};
+
 $(document).ready(main);
