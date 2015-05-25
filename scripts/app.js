4e3d1a55643ada1748b90af9e4ecf04672efad04
Maxim Nelipin <nelipin.maxim@yandex.ru>
2015-05-25 17:56:22 +0300

    Добавил новую вкладку с тегами
    Теги отображаются отдельно, род ними идкт задания
    
    Подправил стиль h3, изменил цвета на менее кислотные

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,18 @@
-var main = function () {
+var main = function (toDoObjects) {
 	"use strict";
-	var toDos = [
-				"Закончить писать эту книгу",
-				"Вывести Грейси на прогулку в парк",
-				"Ответить на электронные письма",
-				"Подготовиться к лекции в понедельник",
-				"Обновить несколько новых задач",
-				"Купить продукты"
-				];
+	var toDos = toDoObjects.map(function (toDo){
+		//получаем из JSON описание задачи и переводим его в массив строк
+		return toDo.description;
+		
+	});
+	//var toDos = [
+			//	"Закончить писать эту книгу",
+			//	"Вывести Грейси на прогулку в парк",
+			//	"Ответить на электронные письма",
+			//	"Подготовиться к лекции в понедельник",
+			//	"Обновить несколько новых задач",
+			//	"Купить продукты"
+			//	];
 	$(".tabs a span").toArray().forEach(function (element){
 		//обработчик щелчка
 		$(element).on("click", function () {
@@ -33,7 +38,49 @@ var main = function () {
 					});
 					
 				}
-				else if ($element.parent().is(":nth-child(3)")) {
+				else if ($element.parent().is(":nth-child(3)")){
+					var organizedByTag = [
+							{
+							"name": "покупки",
+							"toDos": ["Купить продукты"]
+							},
+							{
+							"name": "рутина",
+							"toDos": ["Купить продукты", "Вывести Грейси на прогулку в парк"]
+							},
+							{
+							"name": "писательство",
+							"toDos": ["Сделать несколько новых задач", "Закончить писать книгу"]
+							},
+							{
+
+							"name": "работа",
+									"toDos":  ["Сделать  несколько  новых  задач",  "Подготовиться  к  лекции в понедельник","Ответить на электронные письма", "Закончить писать книгу"]
+							},
+							{
+							"name": " преподавание",
+							"toDos": ["Подготовиться к лекции в понедельник"]
+							},
+							{
+							"name": "питомцы",
+							"toDos": ["Вывести Грейси на прогулку в парк "]
+							}
+						]
+						organizedByTag.forEach(function (tag) {
+							var $tagName=$("<h3/>").text(tag.name),
+								$content=$("<ul/>");
+							tag.toDos.forEach(function(description){
+								var $li=$("<li/>").text(description);
+								$content.append($li);								
+							});
+							$("main .content").append($tagName).append($content);
+							
+						});
+						
+						
+				
+				}
+				else if ($element.parent().is(":nth-child(4)")) {
 					//на 3 вкладке формируем поле и кнопку для добавления заданий
 						var $input=$("<input/>",
 						{
@@ -59,4 +106,9 @@ var main = function () {
 		$(".tabs a:first-child span").trigger("click");
 	};
 
-$(document).ready(main);
+$(document).ready( function (){
+	$.getJSON("todos.json", function (toDoObjects) {
+		//передаём в main задачи
+		main(toDoObjects);
+	});
+});
